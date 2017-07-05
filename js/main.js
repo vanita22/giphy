37f260a0b76717304a051cdb9dc841d2351b7780
vanita22 <vanitayrafita2004@gmail.com>
2017-07-05 00:00:37 -0300

    subiendo archivos

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	var dibujarGifs = function(data){
+		var gif = " ";
+		var url = " ";
+		data.forEach(function(element){
+			gif = element.images.downsized_large.url;
+			url = element.bitly_gif_url;
+		$("#elementos").append(armarTemplate(gif, url));
+		})
+	}
+
+	var armarTemplate = function(gif,url){
+		var t = "<div class='elemento'><img src='" + gif + "'/><a href='" + url + "'>Ver más</a></div>";
+		return t;
+	}
+
+	var ajaxGif = function(gif){
+		$.ajax({
+			url: 'http://api.giphy.com/v1/gifs/search',
+			type: 'GET',
+			datatype: 'json',
+			data: {
+				q: gif,
+				api_key: 'dc6zaTOxFJmzC'
+			}
+		})
+		.done(function(response){
+			console.log(response);
+			dibujarGifs(response.data);
+		})
+		.fail(function(){
+			console.log("error");
+		});
+	}
+
+	$("#buscar-gif").click(function(event){
+		console.log("Entro");
+		$("#elementos").empty();
+		var gif = $("#gif-text").val();
+		ajaxGif(gif);
+	});
+})
